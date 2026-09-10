45e0d12ee857b3e04357a912fa4e45d6a9e3a228
swafvanSFK <safuswafvan1@gmail.com>
2026-09-10 21:30:12 +0400

    changed applied

--- services/chat/routes/chat.routes.js
+++ services/chat/routes/chat.routes.js
@@ -4,7 +4,7 @@ import { createConversation, getConversations, getMessages, saveMessage, updateC
 const router = express.Router()
 
 router.get('/create-conversation', createConversation)
-router.get('/get-conversation', getConversations)
+router.get('/get-conversations', getConversations)
 router.post('/update-conversation', updateConversation)
 router.post('/save-message', saveMessage)
 router.get('/get-message/:conversationId', getMessages)
